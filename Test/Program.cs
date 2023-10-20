634a531bb5e824b8b0cac94e5484244dbf7ac3e7
Benjy <108727379+Benjy0812@users.noreply.github.com>
2023-10-20 20:56:34 +0200

    update made calculator better

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-         
+
 
             /*Console.WriteLine("whats your name sir:");
             string name = Console.ReadLine();
@@ -38,52 +38,64 @@
 
             int reaminder = friends % 2;*/
 
+            do {
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
 
-            double num1 = 0;
-            double num2 = 0;
-            double result = 0;
+                Console.Write("------------------");
+                Console.Write("Calculator Program");
+                Console.Write("------------------");
 
-            Console.Write("------------------");
-            Console.Write("Calculator Program");
-            Console.Write("------------------");
+                Console.WriteLine("\nEnter number 1:  ");
+                num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("\nEnter number 1:  ");
-            num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter number 2:  ");
+                num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter number 2:  ");
-            num2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter an option");
+                Console.WriteLine("\t+ : Add");
+                Console.WriteLine("\t- : Subtract");
+                Console.WriteLine("\t* : Multiply");
+                Console.WriteLine("\t* : Multiply");
+                Console.WriteLine("\t/ : Divide");
 
-            Console.WriteLine("Enter an option");
-            Console.WriteLine("\t+ : Add");
-            Console.WriteLine("\t- : Subtract");
-            Console.WriteLine("\t* : Multiply");
-            Console.WriteLine("\t* : Multiply");
-            Console.WriteLine("\t/ : Divide");
-
-            switch (Console.ReadLine())
-            {
-                case "+":
-                    result = num1 + num2;
-                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    Console.WriteLine($"Your result: {num1} * {num2} = " + result);
-                    break;
-                case "/":
-                    result = num1 / num2;
-                    Console.WriteLine($"Your result: {num1} / {num2} = " + result);
-                    break;
-                default:
-                    Console.WriteLine("Thats was not a valid option");
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
                         break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Thats was not a valid option");
+                            break;
+                 }
+             Console.WriteLine("Would you like to continue? (Y = yes, N = No)");
+            } while (Console.ReadLine().ToUpper() == "Y");
+
+             Console.WriteLine("Bye");
 
 
-            }
+
+           /*Console.WriteLine("Whats your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Your name is {name}!");
+
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You are:{age} old");*/
         }
     }
 }
