4c37bb1c459eaf2df1f1744a8cd0c0db60ab19f7
Benjy <108727379+Benjy0812@users.noreply.github.com>
2024-05-01 23:03:45 +0200

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,40 +4,6 @@
     {
         static void Main(string[] args)
         {
-
-
-            /*Console.WriteLine("whats your name sir:");
-            string name = Console.ReadLine();
-            
-            
-            Console.WriteLine("how old are you:");
-            int age = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("whats your gender ?");
-            string gender = Console.ReadLine();
-
-            Console.WriteLine("your name is:" + name);
-            Console.WriteLine("your are " + age + " years old");
-            Console.WriteLine("your gender is:" + gender);*/
-
-            /*int friends = 10;
-
-            friends = friends + 1;
-            friends += 1;
-            friends++;
-
-            friends = friends - 1;
-            friends -= 1;
-            friends--;
-
-            friends = friends * 2;
-            friends *= 2;
-
-            friends = friends / 2;
-            friends /= 2;
-
-            int reaminder = friends % 2;*/
-
             do {
                 double num1 = 0;
                 double num2 = 0;
@@ -90,15 +56,6 @@
             } while (Console.ReadLine().ToUpper() == "Y");
 
             Console.WriteLine("Bye");
-
-
-            /*Console.WriteLine("Whats your name:");
-             string name = Console.ReadLine();
-             Console.WriteLine($"Your name is {name}!");
-
-             Console.WriteLine("How old are you?");
-             int age = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine($"You are:{age} old");*/ 
         }
     }
 }
